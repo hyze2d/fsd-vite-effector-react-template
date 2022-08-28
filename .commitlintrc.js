4ecd5e1386b1e2a1a6ffbf56a9a2039706929eb3
ein1x3x <ein1x3x.dev@gmail.com>
2022-08-28 04:28:44 +0200

    fix: commitlint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,12 +1,5 @@
 module.exports = {
-  plugins: [
-    // 'commitlint-plugin-jira-rules'
-  ],
-  extends: [
-    // 'jira'
-    '@commitlint/config-conventional'
-  ],
-  rules: {
-    // 'jira-task-id-max-length': [2, 'always', 12],
-  }
+  extends: ['@commitlint/config-conventional'],
+  rules: {},
+  plugins: []
 };
